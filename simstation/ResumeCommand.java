0ec6a6b1817b65a0cc1a12d6fac241ff112d1792
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-04-10 15:08:53 -0700

    Update ResumeCommand.java

--- simstation/ResumeCommand.java
+++ simstation/ResumeCommand.java
@@ -6,7 +6,13 @@ public class ResumeCommand extends Command {
     public ResumeCommand(Model model){
         super(model);
     }
+   
     public void execute() {
-        ;
+        Simulation s = (Simulation) model;
+
+        if(s.getAgentsSuspended()){
+            s.resume();
+            s.changed();
+        }
     }
 }
