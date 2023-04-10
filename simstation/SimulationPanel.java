bce7b1561a2b6abc8500be194296312ea2c5f2c9
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-04-10 14:20:12 -0700

    Update SimulationPanel.java

--- simstation/SimulationPanel.java
+++ simstation/SimulationPanel.java
@@ -1,4 +1,31 @@
 package simstation;
 
-public class SimulationPanel {
+import mvc.*;
+import javax.swing.*;
+
+public class SimulationPanel extends AppPanel {
+    public SimulationPanel(AppFactory factory) {
+        super(factory);
+        JButton start = new JButton("Start");
+        start.addActionListener(this);
+        super.addControl(start);
+        JButton suspend = new JButton("Suspend");
+        suspend.addActionListener(this);
+        super.addControl(suspend);
+        JButton resume = new JButton("Resume");
+        resume.addActionListener(this);
+        super.addControl(resume);
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(this);
+        super.addControl(stop);
+        JButton stats = new JButton("Stats");
+        stats.addActionListener(this);
+        super.addControl(stats);
+    }
+
+    public static void main(String[] args) {
+        AppFactory factory = new SimulationFactory();
+        AppPanel panel = new SimulationPanel(factory);
+        panel.display();
+    }
 }
