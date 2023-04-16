365e650b9d851407dc7de28df3cce949f87fb047
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-04-15 20:21:27 -0700

    Update Agent.java

--- simstation/Agent.java
+++ simstation/Agent.java
@@ -8,6 +8,7 @@ public abstract class Agent extends Bean implements Runnable {
     public Heading heading;
     public int xc;
     public int yc;
+    protected Simulation world;
     protected boolean suspended;
     protected boolean stopped;
     protected Thread myThread;
@@ -17,19 +18,35 @@ public abstract class Agent extends Bean implements Runnable {
         suspended = false;
         stopped = false;
         myThread = null;
-        xc = Utilities.rng.nextInt();
-        yc = Utilities.rng.nextInt();
+        xc = Utilities.rng.nextInt(world.SIZE);
+        yc = Utilities.rng.nextInt(world.SIZE);
     }
 
     public void run() {
-        ;
-    }
-
-    public void start() {
-        ;
+        myThread = Thread.currentThread();
+        while(!stopped) {
+            try {
+                update();
+                Thread.sleep(20);
+                checkSuspended();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
     }
 
     public synchronized void suspend() { suspended = true; }
+    
+    private synchronized void checkSuspended() {
+        try {
+            while(!stopped && suspended) {
+                wait();
+                suspended = false;
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
 
     public synchronized void stop() { stopped = true; }
 
@@ -44,12 +61,47 @@ public abstract class Agent extends Bean implements Runnable {
     }
 
     public void move(int steps) {
-        ;
+        if(heading == Heading.NORTH){
+            yc -= steps;
+            if(yc<=0){
+                yc += world.SIZE;
+            }
+        }
+        else if(heading == Heading.SOUTH){
+            yc += steps;
+            if(yc>=world.SIZE){
+                yc -= world.SIZE;
+            }
+        }
+        else if(heading == Heading.EAST){
+            xc += steps;
+            if(xc >= world.SIZE){
+                xc -= world.SIZE;
+            }
+        }
+        else if(heading == Heading.WEST){
+            xc -= steps;
+            if(xc <= 0){
+                xc += world.SIZE;
+            }
+        }
+        world.changed();
     }
 
     public void setAgentColor(Color agentColor) {
         this.agentColor = agentColor;
     }
+    
+    public int distance(Agent a){
+        int distance = (int)(Math.sqrt((a.getY() - yc) * (a.getY() - yc)
+                            + (a.getX() - xc) * (a.getX() - xc)));
+        return distance;
+    }
+    
+    public void setWorld(Simulation s){
+        world = s;
+    }
+    
     public abstract void update() ;
 
 }
