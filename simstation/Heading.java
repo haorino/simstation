0d3cf0b139b4c8a419db2c252eb2cccb0253bb5e
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-04-15 20:23:51 -0700

    Update Heading.java

--- simstation/Heading.java
+++ simstation/Heading.java
@@ -1,7 +1,42 @@
 package simstation;
+import mvc.Utilities;
 
 public class Heading {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
     public static Heading random(){
-        return new Heading();
+        int direction = Utilities.rng.nextInt(4);
+        if (direction == 1){
+            return NORTH;
+        }
+        else if (direction == 2) {
+            return SOUTH;
+        }
+        else if (direction == 3) {
+            return EAST;
+        }
+        else {
+            return WEST;
+        }
+    }
+
+    public static Heading parse(String heading) {
+        if(heading.equalsIgnoreCase("north")) {
+            return NORTH;
+        }
+        else if(heading.equalsIgnoreCase("south")) {
+            return SOUTH;
+        }
+        else if (heading.equalsIgnoreCase("east")) {
+            return EAST;
+        }
+        else if(heading.equalsIgnoreCase("west")) {
+            return WEST;
+        }
+        else {
+            return null;
+        }
     }
 }
