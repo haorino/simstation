1a7261d363456012b79e941245f7740499b3831d
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-04-16 21:52:09 -0700

    Update FlockingSimulation.java

--- flocking/FlockingSimulation.java
+++ flocking/FlockingSimulation.java
@@ -3,29 +3,23 @@ package flocking;
 import mvc.*;
 import simstation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Random;
 
 class Bird extends Agent {
-    int speed = Utilities.rng.nextInt(3)+1;
+    int speed;
 
     public Bird() {
         super();
         heading = Heading.random();
+        speed = Utilities.rng.nextInt(5)+1;
     }
 
     public void update() {
         // Find a random neighbor and copy its speed and heading
-        Agent neighbor = world.getNeighbor(this, 15);
+        Bird neighbor = (Bird) world.getNeighbor(this, 10);
 
-        if(neighbor == null){
-            heading = Heading.random();
-            speed = Utilities.rng.nextInt(10) + 1;
-        }else{
-            Bird b = (Bird)neighbor;
-            heading = b.heading;
-            speed = b.speed;
+        if(neighbor != null){
+            this.heading = neighbor.heading;
+            this.speed = neighbor.speed;
         }
         move(speed);
     }
@@ -33,38 +27,51 @@ class Bird extends Agent {
 
 
 class RandomWalkFactory extends SimulationFactory {
-    public Model makeModel() {
-        return new FlockingSimulation();
-    }
-    public String getTitle() { return "Random Walks";}
+    public Model makeModel() { return new flockingSimulation(); }
+    public String getTitle() { return "Flocking";}
 }
 
-public class FlockingSimulation extends Simulation {
+public class flockingSimulation extends Simulation {
 
     public void populate() {
         for(int i = 0; i < 20; i++)
             addAgent(new Bird());
     }
 
+    @Override
+    public String[] getStats(){
+        int sp_1 = 0, sp_2 = 0, sp_3 = 0, sp_4 = 0, sp_5 = 0;
+        for(Agent a: agents){
+            if(((Bird)a).speed == 1) {
+                sp_1 +=1;
+            }
+            else if(((Bird)a).speed == 2) {
+                sp_2 +=1;
+            }
+            else if(((Bird)a).speed == 3) {
+                sp_3 +=1;
+            }
+            else if(((Bird)a).speed == 4) {
+                sp_4 +=1;
+            }
+            else if(((Bird)a).speed == 5) {
+                sp_5 +=1;
+            }
+
+        }
+
+        String[] stats = new String[5];
+        stats[0] = "#bird @ speed 1 = " + sp_1;
+        stats[1] = "#bird @ speed 2 = " + sp_2;
+        stats[2] = "#bird @ speed 3 = " + sp_3;
+        stats[3] = "#bird @ speed 4 = " + sp_4;
+        stats[4] = "#bird @ speed 5 = " + sp_5;
+        return stats;
+    }
+
     public static void main(String[] args) {
         AppPanel panel = new SimulationPanel(new RandomWalkFactory());
         panel.display();
-    }
-
-    @Override
-    public String[] getStats(){
-        int[] speeds = new int[5];
-        for(Agent a: agents){
-            Bird b = (Bird)a;
-            speeds[b.speed]++;
-        }
-
-        String[] stats = new String[6];
-        for(int i = 0; i < speeds.length; i++){
-            stats[i] = "bird at speed " + i + ": " + speeds[i];
-        }
-        stats[5] = "clock = " + this.getClock();
-        return stats;
     }
 
 }
